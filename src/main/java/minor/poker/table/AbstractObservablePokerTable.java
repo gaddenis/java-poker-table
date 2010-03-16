e53c72d716d2243efbdc9f9c90d0c100a3f2b859
David Minor <daveminor@gmail.com>
2010-03-16 11:25:03 -0700

    further definition of poker table

---
+++ src/main/java/minor/poker/table/AbstractObservablePokerTable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package minor.poker.table;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author David
+ */
+public abstract class AbstractObservablePokerTable extends Observable implements PokerTable {
+
+}
---
+++ src/main/java/minor/poker/table/PlayerInfo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package minor.poker.table;
+
+/**
+ *
+ * @author David
+ */
+public interface PlayerInfo {
+
+}
